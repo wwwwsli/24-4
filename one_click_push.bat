df5e180e08bb92c1d20a8446f66c641a58a82dbb
wwwwsli <2956382110@qq.com>
2026-02-10 22:06:05 +0800

    Auto backup

--- one_click_push.bat
+++ one_click_push.bat
@@ -1,20 +1,15 @@
 @echo off
 chcp 936 >nul
-echo ========================================
-echo Starting Auto Push to GitHub...
-echo ========================================
 
-:: 1. 强制用 Windows 方式启动管家
+echo Starting Auto Push...
+
 powershell -command "Start-Service ssh-agent -ErrorAction SilentlyContinue"
 
-:: 2. 核心修正：注意这里用了反斜杠 \ 并且加了双引号，防止权限拦截
 ssh-add "D:\github_ssh\id_rsa"
 
-:: 3. 提交并推送
 git add .
-git commit -m "update %date% %time%"
+git commit -m "Auto backup"
 git push origin main
 
-echo ========================================
-echo All Done!
+echo Done!
 pause
